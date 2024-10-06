a66d294acd32977e46d871afb2a70de04f398444
AshanSalinda <ashansalinda5@gmail.com>
2024-10-06 14:31:23 +0530

    WebScraper Class

--- Backend/Scraping Service/scrapers/patpat_scraper.py
+++ Backend/Scraping Service/scrapers/patpat_scraper.py
@@ -6,13 +6,14 @@ from .web_scraper import WebScraper
 class PatpatScraper(WebScraper):
     name = "patpat_scraper"
 
-    def __init__(self, url, ad_link_selector, next_button_selector, *args, **kwargs):
-        super(PatpatScraper, self).__init__(url, ad_link_selector, *args, **kwargs)
+    def __init__(self, url, ad_selector, next_selector, *args, **kwargs):
+        self.next_selector = next_selector
+        super(PatpatScraper, self).__init__(url, ad_selector, *args, **kwargs)
 
 
     def get_next_page(self, response):
         try:
-            next_page = response.css('ul.pagination li:last-child a::attr(href)').get()
+            next_page = response.css(self.next_selector).attrib.get('href')
             return next_page if next_page else None
   
         except Exception as e:
@@ -20,32 +21,35 @@ class PatpatScraper(WebScraper):
 
 
     def get_vehicle_info(self, response):
-        vehicle_details = {}
+        try:
+            vehicle_details = {}
 
-        title = response.css('h2.item-title::text').get()
-        price = response.css('p.m-0.col-6.col-sm-7.p-0.m-0 span::text').getall()
-        table_rows = response.css('table.course-info tr')
+            title = response.css('h2.item-title::text').get()
+            price = response.css('p.m-0.col-6.col-sm-7.p-0.m-0 span::text').getall()
+            table_rows = response.css('table.course-info tr')
 
-        if title:
-            vehicle_details['title'] = title.strip()
+            if title:
+                vehicle_details['title'] = title.strip()
 
-        if len(price) == 2:
-            vehicle_details['price'] = price[1].strip()
+            if len(price) == 2:
+                vehicle_details['price'] = price[1].strip()
 
-        for row in table_rows:
-            key = row.css('td.w-25::text').get()
-            value = row.css('td.w-75::text').get()
-            if key and value:
-                vehicle_details[key.strip().replace(':', '')] = value.strip()
+            for row in table_rows:
+                key = row.css('td.w-25::text').get()
+                value = row.css('td.w-75::text').get()
+                if key and value:
+                    vehicle_details[key.strip().replace(':', '')] = value.strip()
 
-        print(vehicle_details)
-        # Print or log vehicle details
+            print(f"{response.meta.get('index')}\t{response.url}")
+
+        except Exception as e:
+            err(f"{response.meta.get('index')}\t{response.url}")
 
 
 def run_scrapy():
-    url = 'https://www.patpat.lk/vehicle?page=580'
-    ad_link_selector = 'div.result-img a::attr(href)'
-    next_button_selector = 'ul.pagination li:last-child a::attr(href)'
+    url = 'https://www.patpat.lk/vehicle?page=585'
+    ad_selector = 'div.result-img a'
+    next_selector = 'ul.pagination li:last-child a'
 
     process = CrawlerProcess(settings={
         "LOG_LEVEL": 'WARNING',
@@ -53,10 +57,8 @@ def run_scrapy():
     })
 
 
-    process.crawl(PatpatScraper, url, ad_link_selector, next_button_selector)
+    process.crawl(PatpatScraper, url, ad_selector, next_selector)
     process.start()
-
-    warn("Scraping completed")
 
 
 if __name__ == '__main__':
--- Backend/Scraping Service/scrapers/web_scraper.py
+++ Backend/Scraping Service/scrapers/web_scraper.py
@@ -1,16 +1,21 @@
 import scrapy
 from utils.logger import info, warn, err
 from datetime import datetime, timezone
+from urllib.parse import urlparse, parse_qs
 
 
 class WebScraper(scrapy.Spider):
-    def __init__(self, url, ad_link_selector, *args, **kwargs):
+    def __init__(self, url, ad_selector, *args, **kwargs):
         super(WebScraper, self).__init__(*args, **kwargs)
         self.start_urls = [url]
-        self.ad_link_selector = ad_link_selector
-        self.started_at = datetime.now()
-        self.all_vehicle_data = []
-        self.page_count = 0
+        self.ad_selector = ad_selector
+
+
+    def start_requests(self):
+        """This is called when the spider starts crawling"""
+
+        for url in self.start_urls:
+            yield scrapy.Request(url, callback=self.scrape, errback=self.on_error)
 
 
     def closed(self, reason):
@@ -22,35 +27,47 @@ class WebScraper(scrapy.Spider):
         time = time_taken.split('.')[0]
         ms = int(time_taken.split('.')[1]) // 1000
 
-        success_requests = self.crawler.stats.get_value('downloader/response_status_count/200', 0)
-        failed_requests = self.crawler.stats.get_value('downloader/response_status_count/500', 0)
-        not_found_requests = self.crawler.stats.get_value('downloader/response_status_count/404', 0)
-        failed_request_other = self.crawler.stats.get_value('downloader/exception_type_count/twisted.internet.error.TimeoutError', 0) 
-        failed_request_network = self.crawler.stats.get_value('downloader/exception_type_count/twisted.internet.error.DNSLookupError', 0) 
-        print(f"Failed requests: {failed_requests}")
-        print(f"Successful requests: {success_requests}")
-        print(f"Not found requests: {not_found_requests}")
-        print(f"Other failed requests: {start_time}")
-        print(f"network errors {finish_time}")
-        print(f"Scraping completed in: {time}.{ms}")
+        stats = {
+            'error_responses': self.crawler.stats.get_value('downloader/response_status_count/500', 0),
+            'success_requests': self.crawler.stats.get_value('downloader/response_status_count/200', 0),
+            'not_found_responses': self.crawler.stats.get_value('downloader/response_status_count/404', 0),
+            'failed_with_timeout': self.crawler.stats.get_value('downloader/exception_type_count/twisted.internet.error.TimeoutError', 0),
+            'failed_with_network_error': self.crawler.stats.get_value('downloader/exception_type_count/twisted.internet.error.DNSLookupError', 0),
+            'time_taken': f"{time}.{ms}"
+        }
+
+        info(stats)
+
+    
+    def on_error(self, failure):
+        """This is called when an error occurs during a request"""
+
+        url = failure.request.url
+        index = failure.request.meta.get('index', '0:0')
+        err(f"{index}\t{url}")
 
 
-    def parse(self, response):
-        """This is the default callback for every request made by the spider"""
+    def scrape(self, response):
+        """This is the default callback for every request for pages, made by the spider"""
+        try:
+            # Extract the 'page' parameter value from the url
+            page_no = parse_qs(urlparse(response.url).query).get('page', [1])[0] 
+            info(f"Scraping page: {page_no}")
 
-        try:       
-            info(f"Scraping page: {self.page_count}")
-
-            ad_links = response.css('div.result-img a::attr(href)').getall()
-            for link in ad_links:
-                yield response.follow(link, callback=self.get_vehicle_info)
+            # Extract All Ads links and get ad details
+            ad_links = response.css(f"{self.ad_selector}::attr(href)").getall()
+            for index, link in enumerate(ad_links):
+                yield response.follow(
+                    link, 
+                    callback=self.get_vehicle_info, 
+                    errback=self.on_error, 
+                    meta={'index': f"{page_no}:{index + 1}"}
+                )
 
             # Handle pagination
             next_page = self.get_next_page(response)
             if next_page:
-                yield response.follow(next_page, callback=self.parse)
-
-            self.page_count += 1
+                yield response.follow(next_page, callback=self.scrape, errback=self.on_error)
         
         except Exception as e:
             err(f"An error occurred during scraping: {e}")
