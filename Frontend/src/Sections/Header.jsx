e6befe97d7f8402bf67643da48be447bbfb7ebf8
AshanSalinda <ashansalinda5@gmail.com>
2024-10-22 03:04:27 +0530

    Admin Layout Updated

--- Frontend/src/Sections/Header.jsx
+++ Frontend/src/Sections/Header.jsx
@@ -3,9 +3,9 @@ import Logo from "/EstiMotor.svg";
 
 export default function Header({ title }) {
     return (
-        <div className="relative flex items-center justify-center w-screen h-12 px-2 bg-stone-800">
+        <div className="relative flex items-center justify-center w-screen px-2 h-14 bg-dark-300">
             <img src={Logo} alt="EstiMotor" className="absolute w-24 h-auto left-2"/>
-            <h1 className="text-2xl font-bold text-white">{ title }</h1>
+            <h1 className="text-xl font-normal text-dark-100">{ title }</h1>
         </div>
     );
 }
--- Frontend/src/Sections/SideNavbar.jsx
+++ Frontend/src/Sections/SideNavbar.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function SideNavbar() {
   return (
-    <div className='w-12 h-full bg-slate-800'></div>
+    <div className='h-full w-14 bg-dark-400'></div>
   )
 }
