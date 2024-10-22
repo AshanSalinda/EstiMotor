2e14d628176100a208a45ec26af5084d4e29287f
AshanSalinda <ashansalinda5@gmail.com>
2024-10-23 01:48:58 +0530

    Stepper Styles

--- Frontend/src/Components/Stepper/Stepper.jsx
+++ Frontend/src/Components/Stepper/Stepper.jsx
@@ -1,81 +1,80 @@
-import { useState } from 'react';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import StepContent from '@mui/material/StepContent';
+import { useState } from "react";
+import { Step, StepButton, StepLabel, StepContent, Stepper as MuiStepper } from "@mui/material";
+
 
 const steps = [
     {
-      label: 'Select campaign settings',
-      description: `For each ad campaign that you create, you can control how much
-                you're willing to spend on clicks and conversions, which networks
-                and geographical locations you want your ads to show on, and more.`,
+        label: "Select campaign settings",
+        description: `For each ad campaign that you create, you can control how much
+            you're willing to spend on clicks and conversions, which networks
+            and geographical locations you want your ads to show on, and more.`,
     },
     {
-      label: 'Create an ad group',
-      description:
-        'An ad group contains one or more ads which target a shared set of keywords.',
+        label: "Create an ad group",
+        description:
+            "An ad group contains one or more ads which target a shared set of keywords.",
     },
     {
-      label: 'Create an ad',
-      description: `Try out different ad text to see what brings in the most customers,
+        label: "Create an ad",
+        description: `Try out different ad text to see what brings in the most customers,
                 and learn how to enhance your ads using features like ad extensions.
                 If you run into any problems with your ads, find out how to tell if
                 they're running and how to resolve approval issues.`,
     },
-  ];
+];
 
-
-  export default function Stepper1() {
+export default function MyStepper() {
     const [activeStep, setActiveStep] = useState(0);
-  
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    const [expanded, setExpanded] = useState(0)
+
+
+    const handleNext = (index) => {
+        setActiveStep(index + 1);
+        setExpanded(index + 1);
     };
-  
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
+
+    const handleClick = (index) => {
+        setExpanded(index);
     };
-  
+
     const handleReset = () => {
-      setActiveStep(0);
+        setActiveStep(0);
     };
-  
+
     return (
-      <div className='max-w-96'>
-        <Stepper activeStep={activeStep} orientation="vertical">
-          {steps.map((step, index) => (
-            <Step key={step.label}>
-              <StepLabel
-                optional={
-                  index === steps.length - 1 ? (
-                    <span >Last step</span>
-                  ) : <span >A Step</span>
-                }
-              >
-                {step.label}
-              </StepLabel>
-              <StepContent>
-                <span>{step.description}</span>
-                <div className='mb-2'>
-                  <button
-                    onClick={handleNext}
-                    className='mt-1 mr-1'
-                  >
-                    {index === steps.length - 1 ? 'Finish' : 'Continue'}
-                  </button>
-                  <button
-                    disabled={index === 0}
-                    onClick={handleBack}
-                    className='mt-1 mr-1'
-                  >
-                    Back
-                  </button>
-                </div>
-              </StepContent>
-            </Step>
-          ))}
-        </Stepper>
-      </div>
+        <div className="max-w-96">
+            <MuiStepper activeStep={activeStep} orientation="vertical">
+                {(steps || []).map((step, index) => (
+                    <Step key={index}  expanded={expanded == index}>
+                        <StepButton onClick={() => handleClick(index)} >{step.label}</StepButton>
+                        <StepContent>
+                            { expanded == index && (
+                            <Content onNext={handleNext} index={index}>
+                                {step.description}
+                            </Content>
+                            )}
+                        </StepContent>
+                    </Step>
+                ))}
+            </MuiStepper>
+        </div>
     );
-  }
+}
+
+
+function Content({ onNext, index, children }) {
+    return (
+        <div>
+            <div>{children}</div>
+            <div>
+                <button
+                    onClick={() => onNext(index)}
+                    disabled={false}
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                >
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+}
--- Frontend/src/utils/muiTheme.js
+++ Frontend/src/utils/muiTheme.js
@@ -1,20 +1,48 @@
-import { createTheme } from '@mui/material/styles';
-import resolveConfig from 'tailwindcss/resolveConfig';
-import tailwindConfig from '/tailwind.config.js';
+import { createTheme } from "@mui/material/styles";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "/tailwind.config.js";
 
 const fullConfig = resolveConfig(tailwindConfig);
-const colors = fullConfig.theme.colors
-const font = fullConfig.theme.fontFamily.sans.join(',')
+const colors = fullConfig.theme.colors;
+const font = fullConfig.theme.fontFamily.sans.join(",");
+const stepperStyles = {
+    MuiStepIcon: {
+        styleOverrides: {
+            root: { fontSize: "30px" },
+        },
+    },
+    MuiStepConnector: {
+        styleOverrides: {
+            root: { marginLeft: "15px" },
+        },
+    },
+    MuiStepContent: {
+        styleOverrides: {
+            root: { marginLeft: "15px" },
+        },
+    },
+    MuiStepLabel: {
+        styleOverrides: {
+            label: { fontSize: "20px" },
+        },
+    },
+}
+
+
 
 const muiTheme = createTheme({
     palette: {
         primary: { main: colors.primary[500] },
         secondary: { main: colors.primary[900] },
-        grey: { 400: '#ff0000' }
-      },
-      typography: {
+        text: { primary: "#FFFFFF", secondary: "#FFFFFF", disabled: "#FFFFFF" },
+        grey: { 400: "#ff0000" },
+    },
+    typography: {
         fontFamily: font,
-      },
-})
+    },
+    components: {
+        ...stepperStyles,
+    },
+});
 
 export default muiTheme;
