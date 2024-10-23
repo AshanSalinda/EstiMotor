7ccd85e9e0a52625313d11712b57729c974c92ca
AshanSalinda <ashansalinda5@gmail.com>
2024-10-23 15:36:01 +0530

    Stepper Collapse Problem Solved

--- Frontend/src/Components/Stepper/Stepper.jsx
+++ Frontend/src/Components/Stepper/Stepper.jsx
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import { Step, StepButton, StepContent, Stepper as MuiStepper } from "@mui/material";
+import { 
+    Collapse, 
+    Step, 
+    StepLabel,
+    StepContent, 
+    Stepper as MuiStepper 
+} from "@mui/material";
 
 
 export default function MyStepper({ steps, activeStep }) {
     const [expanded, setExpanded] = useState(0)
-    const handleClick = (index) => { setExpanded(index); };
+
+    const handleClick = (index) => {
+        if(index > activeStep){ return };
+        setExpanded(index); 
+    };
+
     useEffect(() => { setExpanded(activeStep) }, [activeStep])
 
     return (
         <div className="max-w-96">
             <MuiStepper activeStep={activeStep} orientation="vertical">
-            {(steps || []).map((step, index) => (
-                <Step key={index}  expanded={expanded == index}>
-                    <StepButton onClick={() => handleClick(index)} icon={null} >{step.label}</StepButton>
-                    <StepContent>{ expanded == index && step.content }</StepContent>
-                </Step>
-            ))}
+                {(steps || []).map((step, index) => (
+
+                    <Step key={index}  expanded={expanded == index} >
+                        <StepLabel 
+                            error={step.error} 
+                            className={index <= activeStep && 'cursor-pointer'} 
+                            onClick={() => handleClick(index)} icon={null} >
+                            {step.label}
+                        </StepLabel>
+
+                        <StepContent >
+                            <Collapse 
+                                in={expanded === index} 
+                                timeout="auto" 
+                                unmountOnExit >
+                                { step.content }
+                            </Collapse>
+                        </StepContent>
+                    </Step>
+                ))}
             </MuiStepper>
         </div>
     );
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,34 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        dark: {
-          100: '#827E7E',
-          200: '#666666',
-          300: '#2B2B2B',
-          400: '#1B1B1B',
-          500: '#171717',
-          600: '#151515',
-          700: '#080808'
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter", "sans-serif"],
+            },
+            colors: {
+                dark: {
+                    100: "#827E7E",
+                    200: "#666666",
+                    300: "#2B2B2B",
+                    400: "#1B1B1B",
+                    500: "#171717",
+                    600: "#151515",
+                    700: "#080808",
+                },
+                primary: {
+                    100: "#F1F6F9",
+                    200: "#D9EAF3",
+                    300: "#B3D4E7",
+                    400: "#4D9FDC",
+                    500: "#0077CC",
+                    600: "#0065A2",
+                    700: "#004669",
+                    800: "#003555",
+                    900: "#002641",
+                },
+            },
+            animation: {
+                "shrink-fade": "shrink-and-fade 0.1s ease-in-out forwards",
+                "grow-fade": "grow-and-fade 0.5s ease-in-out forwards",
+            },
+            keyframes: {
+                "shrink-and-fade": {
+                    "0%": { opacity: "1", height: "auto" },
+                    "100%": { opacity: "0", height: "0" },
+                },
+                "grow-and-fade": {
+                    "0%": { opacity: "0", height: "0" },
+                    "100%": { opacity: "1", height: "auto" },
+                },
+            },
         },
-        primary: {
-          100: '#F1F6F9',
-          200: '#D9EAF3',
-          300: '#B3D4E7',
-          400: '#4D9FDC',
-          500: '#0077CC',
-          600: '#0065A2',
-          700: '#004669',
-          800: '#003555',
-          900: '#002641',
-        },
-      }
     },
-  },
-  plugins: [],
+    plugins: [],
 };
