710e2b17a7fc1fdad7d26b25f375f9449818b9c8
ashan <ashansalinda5@gmail.com>
2025-09-12 18:23:32 +0530

    Return dummy similar Ads with predictions

--- Backend/ML Service/app/main.py
+++ Backend/ML Service/app/main.py
@@ -15,7 +15,7 @@ async def on_shutdown():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
---
+++ Backend/ML Service/app/service/get_ads.py
@@ -0,0 +1,74 @@
+ads = [
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    },
+    {
+        "image": 'https://riyasewana.com/uploads/toyota-raize-1211102822521.jpg',
+        "title": 'Toyota RAIZE 2024 SUV Unregistered (Recondition)',
+        "year": 2020,
+        "mileage": 0,
+        "source": 'Riyasewana.lk',
+        "price": 0,
+        "url": 'https://riyasewana.com/buy/toyota-raize-sale-nittambuwa-10377793'
+    }
+]
